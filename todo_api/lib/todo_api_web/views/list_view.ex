df62af69a53a2d824aeea6ecf89982e086d4beec
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-10 13:11:52 +0800

    updated the list view

--- todo_api/lib/todo_api_web/views/list_view.ex
+++ todo_api/lib/todo_api_web/views/list_view.ex
@@ -1,20 +1,21 @@
 defmodule TodoApiWeb.ListView do
   use TodoApiWeb, :view
-  alias TodoApiWeb.ListView
+  alias TodoApiWeb.{ListView, TodoView}
 
   def render("index.json", %{lists: lists}) do
-    %{data: render_many(lists, ListView, "list.json")}
+    %{data: render_many(lists, ListView, "list_with_todos.json")}
   end
 
   def render("show.json", %{list: list}) do
-    %{data: render_one(list, ListView, "list.json")}
+    %{data: render_one(list, ListView, "list_with_todos.json")}
   end
 
-  def render("list.json", %{list: list}) do
+  def render("list_with_todos.json", %{list: list}) do
     %{
       id: list.id,
       user_id: list.user_id,
-      list_name: list.list_name
+      list_name: list.list_name,
+      todos: render_many(list.todos, TodoView, "todo.json")
     }
   end
 end
