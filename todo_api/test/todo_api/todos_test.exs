e63ebbcc0cd9a5f7776508afb33879a6704d66d7
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-12 17:14:21 +0800

    updated the todos test

--- todo_api/test/todo_api/todos_test.exs
+++ todo_api/test/todo_api/todos_test.exs
@@ -2,6 +2,7 @@ defmodule TodoApi.TodosTest do
   use TodoApi.DataCase
 
   alias TodoApi.Todos
+  alias TodoApi.Lists
 
   describe "todos" do
     alias TodoApi.Todos.Todo
@@ -9,24 +10,25 @@ defmodule TodoApi.TodosTest do
     import TodoApi.TodosFixtures
     import TodoApi.ListsFixtures
 
-    @invalid_attrs %{description: nil, is_done: nil, priority: nil}
-    @valid_attrs %{description: "some description", is_done: true, priority: 1}
-    @out_of_range_attrs %{description: "some description", is_done: true, priority: 100}
+    @invalid_attrs %{"description" => nil, "is_done" => nil, "priority" => nil}
+    @valid_attrs %{"description" => "some description", "is_done" => true, "priority" => 1}
+    @out_of_range_attrs %{
+      "description" => "some description",
+      "is_done" => true,
+      "priority" => 100
+    }
 
     test "list_todos/0 returns all todos" do
-      todo = todo_fixture()
-      assert Todos.list_todos() == {:ok, [todo]}
+      list = list_fixture()
+      todo = todo_fixture(list)
+      assert Todos.list_todos(list) == {:ok, [todo]}
     end
 
     test "get_todo_by_id/1 returns the todo with given id" do
-      todo = todo_fixture()
-      assert Todos.get_todo_by_id(todo.id) == {:ok, todo}
-    end
-
-    test "get_todo_by_priority/1 returns the todo with given id" do
-      todo = todo_fixture()
-      assert Todos.get_todo_by_priority(todo.priority) == {:ok, todo}
-      assert todo.priority == 1
+      list = list_fixture()
+      todo = todo_fixture(list)
+      {:ok, updated_list} = Lists.get_list_by_id(list.id)
+      assert List.first(updated_list.todos, 1) == todo
     end
 
     test "create_todo/1 with valid data creates a todo" do
@@ -50,55 +52,40 @@ defmodule TodoApi.TodosTest do
     end
 
     test "update_todo/2 with valid data updates the todo" do
-      todo = todo_fixture()
+      list = list_fixture()
+      todo = todo_fixture(list)
       update_attrs = %{description: "some updated description", is_done: false}
 
-      assert {:ok, %Todo{} = todo} = Todos.update_todo(todo, update_attrs)
+      assert {:ok, %Todo{} = todo} = Todos.update_todo(list, todo, update_attrs)
       assert todo.description == "some updated description"
       assert todo.is_done == false
     end
 
     test "update_todo/2 with invalid data returns error" do
-      todo = todo_fixture()
-      assert {:error, "Cannot update the todo"} = Todos.update_todo(todo, @invalid_attrs)
-      assert {:ok, todo} == Todos.get_todo_by_id(todo.id)
+      list = list_fixture()
+      todo = todo_fixture(list)
+      assert {:error, "Cannot update the todo"} = Todos.update_todo(list, todo, @invalid_attrs)
     end
 
     test "update_todo/2 with invalid priority returns error" do
-      todo = todo_fixture()
+      list = list_fixture()
+      todo = todo_fixture(list)
 
-      assert {:error, "Assigned 'priority' is out of valid range"} =
-               Todos.update_todo(todo, @out_of_range_attrs)
-
-      assert {:ok, todo} == Todos.get_todo_by_id(todo.id)
-    end
-
-    test "update_todo_by_priority/2 with valid data updates the todo" do
-      todo = todo_fixture()
-      update_attrs = %{description: "some updated description", is_done: false}
-
-      assert {:ok, %Todo{} = todo} = Todos.update_todo_by_priority(todo.priority, update_attrs)
-      assert todo.description == "some updated description"
-      assert todo.is_done == false
+      assert {:error, "Assigned 'priority' is out of valid range"} ==
+               Todos.update_todo(list, todo, @out_of_range_attrs)
     end
 
     test "delete_todo/1 deletes the todo" do
-      todo = todo_fixture()
-      assert {:ok, "'some description' todo is deleted"} = Todos.delete_todo(todo)
-      assert {:not_found, "Todo not found"} == Todos.get_todo_by_id(todo.id)
-    end
+      list = list_fixture()
+      todo = todo_fixture(list)
 
-    test "delete_todo_by_priority/1 deletes the todo by priority" do
-      todo = todo_fixture()
-
-      assert {:ok, "'some description' todo is deleted"} =
-               Todos.delete_todo_by_priority(todo.priority)
-
-      assert {:not_found, "Todo not found"} == Todos.get_todo_by_id(todo.id)
+      assert {:ok, "'some description' todo is deleted"} = Todos.delete_todo(list, todo)
+      assert {:not_found, "Todo not found"} == Todos.get_todo_by_id(list, todo.id)
     end
 
     test "change_todo/1 returns a todo changeset" do
-      todo = todo_fixture()
+      list = list_fixture()
+      todo = todo_fixture(list)
       assert %Ecto.Changeset{} = Todos.change_todo(todo)
     end
   end
