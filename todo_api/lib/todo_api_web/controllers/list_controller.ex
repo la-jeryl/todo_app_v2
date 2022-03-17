2a16216fc8979a619c59067aec46b1763cc3d934
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-17 14:01:57 +0800

    added api authorization checks for lists and todos controller

--- todo_api/lib/todo_api_web/controllers/list_controller.ex
+++ todo_api/lib/todo_api_web/controllers/list_controller.ex
@@ -7,8 +7,9 @@ defmodule TodoApiWeb.ListController do
   action_fallback TodoApiWeb.FallbackController
 
   def index(conn, _params) do
-    with {:ok, lists} <- Lists.list_lists() do
-      render(conn, "index.json", lists: lists)
+    with {:ok, lists} <- Lists.list_lists(),
+         filtered_user_lists <- Enum.filter(lists, &(&1.user_id == conn.assigns.current_user.id)) do
+      render(conn, "index.json", lists: filtered_user_lists)
     else
       {:error, reason} ->
         conn |> put_status(:bad_request) |> render("error.json", error: reason)
@@ -16,41 +17,77 @@ defmodule TodoApiWeb.ListController do
   end
 
   def create(conn, %{"list" => list_params}) do
-    with {:ok, %List{} = list} <- Lists.create_list(list_params) do
+    with true <- list_params["user_id"] == conn.assigns.current_user.id,
+         {:ok, %List{} = list} <- Lists.create_list(list_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.list_path(conn, :show, list))
       |> render("show.json", list: list)
     else
-      {_, reason} -> conn |> put_status(:bad_request) |> render("error.json", error: reason)
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error: "Logged in user is not allowed to create a todo list for a different user"
+        )
+
+      {_, reason} ->
+        conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 
   def show(conn, %{"id" => id}) do
-    with {:ok, list} <- Lists.get_list_by_id(id) do
+    with {:ok, list} <- Lists.get_list_by_id(id),
+         true <- list.id == conn.assigns.current_user.id do
       render(conn, "show.json", list: list)
     else
-      {_, reason} -> conn |> put_status(:bad_request) |> render("error.json", error: reason)
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error: "Logged in user is not allowed to access a todo list from a different user"
+        )
+
+      {_, reason} ->
+        conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 
   def update(conn, %{"id" => id, "list" => list_params}) do
-    with {:ok, list} <- Lists.get_list_by_id(id),
+    with true <- list_params["user_id"] == conn.assigns.current_user.id,
+         {:ok, list} <- Lists.get_list_by_id(id),
          {:ok, %List{} = list} <- Lists.update_list(list, list_params) do
       render(conn, "show.json", list: list)
     else
-      {_, reason} -> conn |> put_status(:bad_request) |> render("error.json", error: reason)
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error: "Logged in user is not allowed to update a todo list from a different user"
+        )
+
+      {_, reason} ->
+        conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 
   def delete(conn, %{"id" => id}) do
     with {:ok, list} <- Lists.get_list_by_id(id),
+         true <- list.user_id == conn.assigns.current_user.id,
          {:ok, _list} <- Lists.delete_list(list) do
       render(conn, "delete_list_with_todos.json",
         message: "'#{list.list_name}' todo list was deleted."
       )
     else
-      {_, reason} -> conn |> put_status(:bad_request) |> render("error.json", error: reason)
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error: "Logged in user is not allowed to delete a todo list from a different user"
+        )
+
+      {_, reason} ->
+        conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 end
--- todo_api/lib/todo_api_web/controllers/todo_controller.ex
+++ todo_api/lib/todo_api_web/controllers/todo_controller.ex
@@ -9,52 +9,99 @@ defmodule TodoApiWeb.TodoController do
 
   def index(conn, %{"list_id" => list_id}) do
     with {:ok, todo_list} <- Lists.get_list_by_id(list_id),
+         true <- todo_list.user_id == conn.assigns.current_user.id,
          sorted_list <- Enum.sort_by(todo_list.todos, & &1.priority) do
       render(conn, "index.json", todos: sorted_list)
     else
-      {_, reason} -> conn |> put_status(:bad_request) |> render("error.json", error: reason)
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error:
+            "Logged in user is not allowed to access all todos from a different user's todo list"
+        )
+
+      {_, reason} ->
+        conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 
   def create(conn, %{"list_id" => list_id, "todo" => todo_params}) do
-    with {:ok, list} <- Lists.get_list_by_id(list_id),
-         {:ok, %Todo{} = todo} <- Todos.create_todo(list, todo_params) do
+    with {:ok, todo_list} <- Lists.get_list_by_id(list_id),
+         true <- todo_list.user_id == conn.assigns.current_user.id,
+         {:ok, %Todo{} = todo} <- Todos.create_todo(todo_list, todo_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.list_todo_path(conn, :show, list_id, todo))
       |> render("show.json", todo: todo)
     else
-      {_, reason} -> conn |> put_status(:bad_request) |> render("error.json", error: reason)
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error:
+            "Logged in user is not allowed to create a todo from a different user's todo list"
+        )
+
+      {_, reason} ->
+        conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 
   def show(conn, %{"list_id" => list_id, "id" => id}) do
-    with {:ok, list} <- Lists.get_list_by_id(list_id),
-         {:ok, todo} <- Todos.get_todo_by_id(list, id) do
+    with {:ok, todo_list} <- Lists.get_list_by_id(list_id),
+         true <- todo_list.user_id == conn.assigns.current_user.id,
+         {:ok, todo} <- Todos.get_todo_by_id(todo_list, id) do
       render(conn, "show.json", todo: todo)
     else
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error:
+            "Logged in user is not allowed to access a todo from a different user's todo list"
+        )
+
       {_, reason} ->
         conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 
   def update(conn, %{"list_id" => list_id, "id" => id, "todo" => todo_params}) do
-    with {:ok, list} = Lists.get_list_by_id(list_id),
-         {:ok, todo} <- Todos.get_todo_by_id(list, id),
-         {:ok, %Todo{} = todo} <- Todos.update_todo(list, todo, todo_params) do
+    with {:ok, todo_list} = Lists.get_list_by_id(list_id),
+         true <- todo_list.user_id == conn.assigns.current_user.id,
+         {:ok, todo} <- Todos.get_todo_by_id(todo_list, id),
+         {:ok, %Todo{} = todo} <- Todos.update_todo(todo_list, todo, todo_params) do
       render(conn, "show.json", todo: todo)
     else
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error:
+            "Logged in user is not allowed to update a todo from a different user's todo list"
+        )
+
       {_, reason} ->
         conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
   end
 
   def delete(conn, %{"list_id" => list_id, "id" => id}) do
-    with {:ok, list} = Lists.get_list_by_id(list_id),
-         {:ok, todo} <- Todos.get_todo_by_id(list, id),
-         {:ok, _todo} <- Todos.delete_todo(list, todo) do
+    with {:ok, todo_list} = Lists.get_list_by_id(list_id),
+         true <- todo_list.user_id == conn.assigns.current_user.id,
+         {:ok, todo} <- Todos.get_todo_by_id(todo_list, id),
+         {:ok, _todo} <- Todos.delete_todo(todo_list, todo) do
       render(conn, "delete_todo.json", message: "'#{todo.title}' todo was deleted.")
     else
+      false ->
+        conn
+        |> put_status(:bad_request)
+        |> render("error.json",
+          error:
+            "Logged in user is not allowed to delete a todo from a different user's todo list"
+        )
+
       {_, reason} ->
         conn |> put_status(:bad_request) |> render("error.json", error: reason)
     end
