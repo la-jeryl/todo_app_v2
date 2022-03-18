ae5579b97bde41ab828d7516030a9d8b7539491b
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-18 16:32:52 +0800

    created registration and session files

---
+++ client/lib/client/registration.ex
@@ -0,0 +1,22 @@
+defmodule Client.Registration do
+  use Tesla
+
+  plug Tesla.Middleware.BaseUrl, "http://localhost:4000/api"
+  plug Tesla.Middleware.JSON
+
+  def register(email, password, password_confirmation) do
+    with true <- password == password_confirmation,
+         {:ok, response} <- post("/registration", %{user: %{email: email, password: password}}) do
+      case Map.has_key?(response.body, "data") do
+        true ->
+          response.body["data"]
+
+        false ->
+          response.body["error"]
+      end
+    else
+      false -> %{"error" => "Passwords do not match"}
+      {:error, reason} -> %{"error" => reason}
+    end
+  end
+end
