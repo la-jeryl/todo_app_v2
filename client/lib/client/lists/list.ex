840d5ec740f9d797bc760cb23a1b55fa4880a5ee
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-22 18:08:48 +0800

    updated the list and todo schema

--- client/lib/client/lists/list.ex
+++ client/lib/client/lists/list.ex
@@ -2,12 +2,9 @@ defmodule Client.Lists.List do
   use Ecto.Schema
   import Ecto.Changeset
 
-  # alias Client.Todos.Todo
-
   schema "lists" do
     field :list_name, :string
     field :user_id, :integer
-    # has_many(:todos, Todo, on_replace: :delete)
 
     timestamps()
   end
@@ -16,7 +13,6 @@ defmodule Client.Lists.List do
   def changeset(list, attrs) do
     list
     |> cast(attrs, [:user_id, :list_name])
-    # |> cast_assoc(:todos)
     |> validate_required([:user_id, :list_name])
   end
 end
--- client/lib/client/todos/todo.ex
+++ client/lib/client/todos/todo.ex
@@ -2,14 +2,11 @@ defmodule Client.Todos.Todo do
   use Ecto.Schema
   import Ecto.Changeset
 
-  # alias Client.Lists.List
-
   schema "todos" do
     field :title, :string
     field :description, :string
     field :is_done, :boolean, default: false
     field :priority, :integer
-    # belongs_to(:list, List)
 
     timestamps()
   end
@@ -19,7 +16,5 @@ defmodule Client.Todos.Todo do
     todo
     |> cast(attrs, [:priority, :title, :description, :is_done])
     |> validate_required([:title, :is_done])
-
-    # |> assoc_constraint(:list)
   end
 end
