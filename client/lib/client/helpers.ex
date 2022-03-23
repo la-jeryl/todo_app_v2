568a85d690b7e5fb05a8dfd451a3f240ce53cb6c
la-jeryl <jeryl.estopace@lawadvisor.com>
2022-03-23 22:41:28 +0800

    minor update on the helper

--- client/lib/client/helpers.ex
+++ client/lib/client/helpers.ex
@@ -2,7 +2,7 @@ defmodule Client.Helpers do
   def keys_to_atoms(string_key_map) when is_map(string_key_map) do
     for {key, val} <- string_key_map,
         into: %{},
-        do: {String.to_existing_atom(key), keys_to_atoms(val)}
+        do: {String.to_atom(key), keys_to_atoms(val)}
   end
 
   def keys_to_atoms(string_key_list) when is_list(string_key_list) do
